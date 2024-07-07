9477a0564e75d038497e76b6a633c8676cf5780f
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-07 22:25:56 +0700

    Simpler CombinedModel

--- src/train.py
+++ src/train.py
@@ -400,7 +400,7 @@ def main():
     elif CONFIG['architecture'] == 'CombinedModel':
         model = CombinedModel(image_model_name=args.model_name,
                               metadata_dim=len(meta_feature_columns) if CONFIG['use_meta'] else 0, 
-                              hidden_dims=[512, 128], 
+                              hidden_dims=[128, 64], 
                               metadata_output_dim=32,
                               use_attention=args.use_attention,
                               attention_type=args.image_attention_type,
