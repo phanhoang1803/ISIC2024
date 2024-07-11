b148c9269488a718a7856bb67ffc89ea9d2ba453
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-11 09:30:39 +0700

    Add posweight

--- src/models/criterion.py
+++ src/models/criterion.py
@@ -74,7 +74,10 @@ def criterion(outputs, targets, pos_weight=20.0, loss='bce_with_logits'):
     if loss == 'bce':
         return nn.BCELoss()(outputs, targets)
     elif loss == 'bce_with_logits':
-        return nn.BCEWithLogitsLoss(pos_weight=torch.tensor([pos_weight]).to(outputs.device))(outputs, targets)
+        if pos_weight > 0:
+            return nn.BCEWithLogitsLoss(pos_weight=torch.tensor([pos_weight]).to(outputs.device))(outputs, targets)
+        else:
+            return nn.BCEWithLogitsLoss()(outputs, targets)
     elif loss == 'focal':
         return FocalLoss()(outputs, targets)
     elif loss == 'pauc':
