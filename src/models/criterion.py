dd1620431229263e0004b4684433243d112ece39
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-11 13:57:44 +0700

    Update focal loss

--- src/models/criterion.py
+++ src/models/criterion.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
 
 class PAUCLoss(nn.Module):
     def __init__(self, min_tpr=0.8):
@@ -42,40 +40,24 @@ class PAUCLoss(nn.Module):
         return loss
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=0.05, size_average=True):
+    def __init__(self, alpha=0.5, gamma=2, reduction='mean'):
         super(FocalLoss, self).__init__()
-        self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha, (float, int)): 
-            self.alpha = torch.tensor([alpha, 1 - alpha])
-        elif isinstance(alpha, list):
-            self.alpha = torch.tensor(alpha)
-        self.size_average = size_average
+        self.gamma = gamma
+        self.reduction = reduction
+                    
+    def forward(self, inputs, targets):
+        BCE_loss = nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
 
-    def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
-
-        target = target.view(-1, 1)
-
-        logpt = F.log_softmax(input, dim=1)  # Ensure to use softmax across the correct dimension
-        logpt = logpt.gather(1, target.long())
-        logpt = logpt.view(-1)
-        pt = logpt.exp()
-
-        if self.alpha is not None:
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1).long())
-            logpt = logpt * at
-
-        loss = -1 * (1 - pt) ** self.gamma * logpt
-        if self.size_average:
-            return loss.mean()
+        if self.reduction == 'mean':
+            return torch.mean(F_loss)
+        elif self.reduction == 'sum':
+            return torch.sum(F_loss)
         else:
-            return loss.sum()
+            return F_loss
+
 def criterion(outputs, targets, pos_weight=20.0, loss='bce_with_logits'):
     """
     Calculate the binary cross entropy loss between the model's outputs and the targets.
