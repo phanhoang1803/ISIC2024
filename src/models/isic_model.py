12aab44b87398239cc8e09e7ebefaf49944d6452
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-13 11:00:09 +0700

    Change ISICModel

--- src/models/isic_model.py
+++ src/models/isic_model.py
@@ -12,15 +12,43 @@ import numpy as np
 class ISICModel(nn.Module):
     def __init__(self, model_name, num_classes=1, pretrained=True, checkpoint_path=None):
         super(ISICModel, self).__init__()
+        self.model_name = model_name
         self.model = getattr(models, model_name)(pretrained=pretrained)
-
-        in_features = self.model.classifier.in_features
+        self.output_dim = self._get_output_dim()
+        
         self.model.classifier = nn.Identity()
-        self.model.global_pool = nn.Identity()
+        self.model.avgpool = nn.Identity()
         self.pooling = GeM()
-        self.linear = nn.Linear(in_features, num_classes)
+        self.linear = nn.Linear(self.output_dim, num_classes)
         self.sigmoid = nn.Sigmoid()
 
+    def _get_output_dim(self):
+        lookup = {
+            "vit_l": 1024,
+            "vit_b": 768,
+            "densenet121": 1024,
+            "swin_b": 1536,
+            "nest_base": 1024,
+            'resnet18': 512,
+            'resnet50': 2048,
+            'vgg16': 4096,
+            'efficientnet_b0': 1280,
+            'efficientnet_b1': 1280,
+            'efficientnet_b2': 1408,
+            'efficientnet_b3': 1536,
+            'efficientnet_b4': 1792,
+            'efficientnet_b5': 2048,
+            'efficientnet_b6': 2304,
+            'efficientnet_b7': 2560,
+            
+        }
+        
+        dim = lookup.get(self.model_name, None)
+        if dim is None:
+            raise ValueError(f"Unsupported model: {self.model_name}\n Supported models: vit_l, vit_b, densenet121, swin_b, nest_base")
+        
+        return dim
+        
     def forward(self, images):
         features = self.model(images)
         pooled_features = self.pooling(features).flatten(1)
